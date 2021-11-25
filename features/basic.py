c5b78828933305a276ff060489405d532df2e18d
Brij Vaghani-iiitdwd <88758778+Brij2003@users.noreply.github.com>
2021-11-25 17:24:38 +0530

    added features

---
+++ features/basic.py
@@ -0,0 +1,9 @@
+def listToString(query):
+    # initialize an empty string
+    str = ""
+    # traverse in the string
+    for ele in query:
+        str += ele
+    # return string
+    return str
+    
