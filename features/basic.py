7cca9b8020188108c8d104962e960e2318eb224d
Brij Vaghani-iiitdwd <88758778+Brij2003@users.noreply.github.com>
2021-12-01 18:40:23 +0530

    Delete features directory

--- features/basic.py
+++
@@ -1,9 +0,0 @@
-def listToString(query):
-    # initialize an empty string
-    str = ""
-    # traverse in the string
-    for ele in query:
-        str += ele
-    # return string
-    return str
-    
